84eb8f77e5fd82f5d71f8dcc853046793643bc51
Ainur Dzholdosheva <ajnurdzoldoseva@gmail.com>
2020-02-10 23:01:01 +0600

    three commits

---
+++ index.js
@@ -0,0 +1,18 @@
+let sections=document.querySelectorAll("section");
+
+let links=document.querySelectorAll("a");
+
+for(let index=0; index<links.length; index++){
+    const element=links[index];
+
+    element.addEventListener("click", function(event){
+        event.preventDefault();
+
+        let blockId=element.getAttribute("href");
+        let section=document.querySelector(blockId);
+        section.scrollIntoView({
+            behavior:"smooth",
+            block:"start"
+        });
+    });
+}
